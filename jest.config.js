a0985c2d71c3602e09b7dc9cc2463cd048809432
Thulasi Ram <thulasi503@gmail.com>
2022-05-10 18:34:49 +0530

    fix: hookup ts-jest in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+    preset: 'ts-jest',
     testEnvironment: 'jest-environment-jsdom',
     testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/dist'],
     moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
     setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
+
 };
